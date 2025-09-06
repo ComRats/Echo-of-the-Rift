25ade609cca9408a74e73918d02bb9b043c436b7
9ikii5ec <niiitakirkizh@gmail.com>
2025-09-06 23:18:26 +0300

    added a key display if necessary

--- Assets/!SeriouslyProject/Scripts/StarCollection/StarTrigger.cs
+++ Assets/!SeriouslyProject/Scripts/StarCollection/StarTrigger.cs
@@ -5,13 +5,24 @@ using UnityEngine.UI;
 public class StarTrigger : MonoBehaviour
 {
     [SerializeField] private Image backPanel;
+    [SerializeField] private Vector3 keyMassageOffset;
 
+    private SpriteCollection sprites;
+    private GameMassage message = new();
     private bool playerInside = false;
+
+    private void Start()
+    {
+        sprites = FindObjectOfType<SpriteCollection>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out var player))
+        {
             playerInside = true;
+            message.ButtonMassage(gameObject, playerInside, sprites.sprites[14], keyMassageOffset);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -20,6 +31,7 @@ public class StarTrigger : MonoBehaviour
         {
             playerInside = false;
             backPanel.gameObject.SetActive(false);
+            message.ButtonMassage(gameObject, playerInside, sprites.sprites[14], keyMassageOffset);
         }
     }
 
---
+++ Assets/!SeriouslyProject/Scripts/UI/GameMassage.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GameMassage
+{
+    private GameObject newMessage;
+
+    public void ButtonMassage(GameObject target, bool isShow, Sprite sprite, Vector3 offset = default)
+    {
+        if (isShow)
+        {
+            if (newMessage != null) return;
+
+            newMessage = new GameObject("KeyMassage");
+            newMessage.transform.position = target.transform.position + offset;
+            newMessage.transform.localScale = new(2f, 2f, 0f);
+            newMessage.transform.SetParent(target.transform);
+
+            var sr = newMessage.AddComponent<SpriteRenderer>();
+            sr.sprite = sprite;
+
+            sr.sortingLayerName = "Player";
+            sr.sortingOrder = 500;
+        }
+        else
+        {
+            if (newMessage != null)
+            {
+                Object.Destroy(newMessage);
+                newMessage = null;
+            }
+        }
+    }
+
+
+}
