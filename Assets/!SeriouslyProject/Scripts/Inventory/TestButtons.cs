fd945715e13ed5a54b0d5875e50e6501bbbfb708
9ikii5ec <niiitakirkizh@gmail.com>
2026-03-30 17:16:21 +0300

    change village location ending
    change debug items for invenory

--- Assets/!SeriouslyProject/Scripts/Inventory/TestButtons.cs
+++ Assets/!SeriouslyProject/Scripts/Inventory/TestButtons.cs
@@ -4,16 +4,16 @@ public class TestButtons : MonoBehaviour
 {
     [SerializeField] private InventoryManager inventoryManager;
 
+#if UNITY_EDITOR
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
             inventoryManager.AddItem("axe", 1);
-            inventoryManager.AddItem("pig", 1);
-            inventoryManager.AddItem("karas", 1);
             inventoryManager.AddItem("testAmulet", 1);
-            inventoryManager.AddItem("testArmor", 1);
-            inventoryManager.AddItem("testHelmet", 1);
+            inventoryManager.AddItem("rustChestplate", 1);
+            inventoryManager.AddItem("brokeHelmet", 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
@@ -22,4 +22,5 @@ public class TestButtons : MonoBehaviour
             inventoryManager.AddItem("karas", 1);
         }
     }
+#endif
 }
